e86c9fc0e8c64cdc7dc003ab277b1e0a7b11248f
Jeff Epler <jepler@gmail.com>
2021-10-25 08:21:33 -0500

    Improve the sig2ms program
    Allow commandline selection of polarity, jitter, timebase, noise,
    and phase.  This makes it easy to demonstrate what signal variations
    the receiver will tolerate.
    
    Make it run faster by using numpy to add noise, instead of dealing with
    each character of output separately.

--- sig2ms.py
+++ sig2ms.py
@@ -22,29 +22,51 @@
 
 import sys
 import random
-NC, C = '10'
+import getopt
+import numpy
 
-def noise_one(c):
-    if random.uniform(0., 1.) > .65: return "01"[not int(c)]
-    return c
+opts, args = getopt.getopt(sys.argv[1:], "ij:t:n:p:")
 
-def noise(s):
-    return s
-    return "".join(noise_one(c) for c in s)
+invert = False
+jitter = 0
+timebase = 1000
+noise = 0
+phase = 0
 
-wf = {}
-wf['0'] = NC * 200 + C * 800
-wf['1'] = NC * 500 + C * 500
-wf['2'] = NC * 800 + C * 200
+T = { '0': .2, '1': .5, '2': .8 }
 
-data = []
-for line in sys.stdin:
-    if not line.startswith("'"): continue
-    line = line.strip().split(None, 2)[2]
-    data.append(line)
-data = '012' * 20 + "".join(data) + '012'
+for k, v in opts:
+    if k == '-i': invert = not invert
+    if k == '-j': jitter = float(v)
+    if k == '-t': timebase = float(v)
+    if k == '-n': noise = float(v)
+    if k == '-p': phase = float(v)
+
+NC, C = '0', '1'
+if invert: C, NC = NC, C
+
+def add_noise(s):
+    if not noise: return s
+    s = numpy.fromstring(s, numpy.int8)
+    r = numpy.random.random(size=(len(s),))
+    return (s ^ (r < noise)).tostring()
+
+t = phase - int(phase) - 1
+
 try:
-    for c in data:
-        sys.stdout.write(noise(wf[c]))
-    sys.stdout.flush()
+    for line in sys.stdin:
+	if not line.startswith("'"): continue
+	line = line.strip().split(None, 2)[2]
+	for c in line:
+	    t1 = random.uniform(-jitter, jitter)
+	    t2 = T[c] + random.uniform(-jitter, jitter)
+	    dt1 = int((t1 - t) * timebase)
+	    t = t + dt1 * 1. / timebase
+	    sys.stdout.write(add_noise(C * dt1))
+	    dt2 = int((t2 - t) * timebase)
+	    sys.stdout.write(add_noise(NC * dt2))
+	    t = t + dt2 * 1. / timebase
+	    t = t - int(t) - 1
+    sys.stdout.write(add_noise(NC * int(timebase)) + "\n")
+
 except (IOError, KeyboardInterrupt): pass
